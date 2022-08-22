c6ba9ea24244a41846cd0eb8c7594b64d5d751c7
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-22 18:06:05 +0530

    ERSrdtrfiytswgb

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -39,19 +39,19 @@ done
 invalid_file_names=()
 for file_name in "${unique_file_names[@]}"; do
     for fileignore in "${file_names_to_ignore[@]}"; do
-        echo "file name is :${file_name}"
-        echo "Ignore: ${fileignore}"
-        if [[ "${file_name}" == "${fileignore}" ]]; then 
-            echo "in contine: ${file_name}"
-            continue
-            echo "continue"
-            if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
+        # echo "file name is :${file_name}"
+        # echo "Ignore: ${fileignore}"
+        # if [[ "${file_name}" == "${fileignore}" ]]; then 
+        #     echo "in contine: ${file_name}"
+        #     continue
+        #     echo "continue"
+            if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ] && [ "${file_name}" == "${fileignore}" ]]; then
                     invalid_file_names+=(${file_name})
-                    
+
                     echo "Invalid FileName : ${file_name}" 
                     exit 1
             fi
-        fi
+    done
         
     done
 
