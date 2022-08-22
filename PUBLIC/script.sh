3278dcdbc227acb921f2d3ec78b5368955cd8929
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-22 19:59:26 +0530

    dsygdys

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -37,7 +37,9 @@ done
 
 invalid_file_names=()
 fileValidator=[0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$
+echo "fileva :${fileValidator}"
 for file_name in "${unique_file_names[@]}"; do
+ echo "filename: ${file_name}"
     for fileignore in "${file_names_to_ignore[@]}"; do
         # echo "file name is :${file_name}"
         # echo "Ignore: ${fileignore}"
