861cffe2babec750330ffa7d559039bee2c19d67
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-22 18:22:25 +0530

    ERSrdtrfiytswgb

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -38,7 +38,6 @@ for dir in "${unique_dirs[@]}"; do
 done  
 invalid_file_names=()
 fileValidator=[0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$
-echo ${fileValidator}
 for file_name in "${unique_file_names[@]}"; do
     for fileignore in "${file_names_to_ignore[@]}"; do
         # echo "file name is :${file_name}"
@@ -47,7 +46,7 @@ for file_name in "${unique_file_names[@]}"; do
         #     echo "in contine: ${file_name}"
         #     continue
         #     echo "continue"
-        if [ ! "${file_name}" =~ "${fileValidator}"] && [ ! "${file_name}" =~ "${fileignore}" ]; then
+        if [[ ! "${file_name}" =~ "${fileValidator}"] && [ ! "${file_name}" =~ "${fileignore}" ]]; then
             invalid_file_names+=(${file_name})            
             echo "Invalid FileName : ${file_name}" 
             exit 1
