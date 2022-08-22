5b41abb984b295fb06010d8927d47c70a9ce33dd
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-22 16:32:53 +0530

    ERSrdtrfiytswgb

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -38,7 +38,11 @@ done
 invalid_file_names=()
 for file_name in "${unique_file_names[@]}"; do
     echo "--------------11111-------------------"
+    echo "${file_name}" 
+    echo "---------------------file_name_test" 
     for file_name_test in "${file_names_to_ignore[@]}";do
+        echo "${file_name_test}" 
+        echo "---------------------file_name_test" 
         if [[ "${file_name}" == "${file_name_test }" ]]; then
             echo "---------------inpom=-------------------"
             echo ${file_name_test}
