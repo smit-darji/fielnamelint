e60583b16ff38b3d987835685170e4cb296fa5d8
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-22 14:44:14 +0530

    ERSrdtrfiytswgb

---
+++ public.sh/script.sh
@@ -0,0 +1,127 @@
+echo "$CHANGED_FILES"
+
+git fetch --no-tags --prune --depth=1 origin +refs/heads/main:refs/remotes/origin/main
+echo "::set-output name=changedfiles::$(git diff --name-only --diff-filter=ACMRT origin/main HEAD)"
+# changedfiles=( $(git diff --name-only --diff-filter=ACMRT origin/main HEAD) )
+changedfiles=( $(git diff --name-only --diff-filter=ACMRT origin/main HEAD | uniq | jq -R -s -c 'split("\n")[:-1]' | jq -r '.[] | @sh' | tr -d \') )
+echo "-----------------------"
+echo ${changedfiles[@]}
+echo "-----------------------"
+
+file_names_to_ignore=("changelog.xml", "pom.xml", "ReadMe.md")
+for i in "${!CHANGED_FILES[@]}"; do
+    if [[ "${CHANGED_FILES[i]}" == .github* ]]; then
+        unset 'CHANGED_FILES[i]'
+        echo "${CHANGED_FILES[i]}"
+    fi
+done
+unique_dirs=()
+unique_file_names=()
+for i in "${!CHANGED_FILES[@]}"; do
+    if [[ ! " ${file_names_to_ignore[*]} " =~ " ${CHANGED_FILES[i]##*/} " ]]; then
+        unique_file_names+=(${CHANGED_FILES[i]##*/})
+    fi
+    IFS='/' read -ra path <<< "${CHANGED_FILES[i]%/*}/"
+    for i in "${path[@]}"; do
+        if [[ ! " ${unique_dirs[*]} " =~ " ${i} " ]]; then
+            unique_dirs+=(${i})
+        fi
+    done
+done
+invalid_dirs=()
+for dir in "${unique_dirs[@]}"; do
+    if [[ ! "${dir}" =~ ^[A-Z0-9._]*$ ]]; then
+        invalid_dirs+=(${dir}) 
+        echo ${dir}
+        echo "Invalid Dirrectory Name"
+        exit 1
+    fi
+done  
+
+invalid_file_names=()
+for file_name in "${unique_file_names[@]}"; do
+    if [[ ! " ${file_names_to_ignore[*]} " =~ " ${CHANGED_FILES[i]##*/} " ]]; then
+        exit 0                           
+    fi
+    if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
+        invalid_file_names+=(${file_name})
+        echo ${file_name}
+        echo "Invalid FileName" 
+        exit 1
+    fi
+
+done 
+
+if [[ ! -z "$invalid_dirs" || ! -z "$invalid_file_names" ]]; 
+    then
+        echo "Failed!!"
+        if [[ ! -z "$invalid_dirs" ]]; then
+            echo "Invalid Directory Names"
+            echo "${invalid_dirs[@]}"
+        fi 
+        if [[ ! -z "$invalid_file_names" ]]; then
+            echo "Invalid File Names"
+            echo "${invalid_file_names[@]}"
+        fi
+    else
+        echo "Success!!"
+fi
+# echo "$CHANGED_FILES"
+# readarray -t changedfiles < <("$CHANGED_FILES")
+# echo "hello"
+# echo "${changedfiles[@]}"
+
+# file_names_to_ignore=("changelog.xml", "pom.xml", "ReadMe.md")
+
+# # Remove files of .github directory from list
+# for i in "${!changedfiles[@]}"; do
+#     if [[ "${changedfiles[i]}" == .github* ]]; then
+#         unset 'changedfiles[i]'
+#     fi
+# done
+
+# # Get unique directories and file names
+# unique_dirs=()
+# unique_file_names=()
+# for i in "${!changedfiles[@]}"; do
+#     if [[ ! " ${file_names_to_ignore[*]} " =~ " ${changedfiles[i]##*/} " ]]; then
+#         unique_file_names+=(${changedfiles[i]##*/})
+#     fi
+#     IFS='/' read -ra path <<< "${changedfiles[i]%/*}/"
+#     for i in "${path[@]}"; do
+#         if [[ ! " ${unique_dirs[*]} " =~ " ${i} " ]]; then
+#             unique_dirs+=(${i})
+#         fi
+#     done
+# done
+
+# # Get Invalid Directory names
+# invalid_dirs=()
+# for dir in "${unique_dirs[@]}"; do
+#     if [[ ! "${dir}" =~ ^[A-Z0-9._]*$ ]]; then
+#         invalid_dirs+=(${dir}) 
+#     fi
+# done
+
+# # Get Invalid Directory names
+# invalid_file_names=()
+# for file_name in "${unique_file_names[@]}"; do
+#     if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
+#         invalid_file_names+=(${file_name}) 
+#     fi
+# done
+
+# if [[ ! -z "$invalid_dirs" || ! -z "$invalid_file_names" ]]; 
+#     then
+#         echo "Failed!!"
+#         if [[ ! -z "$invalid_dirs" ]]; then
+#             echo "Invalid Directory Names"
+#             echo "${invalid_dirs[@]}"
+#         fi 
+#         if [[ ! -z "$invalid_file_names" ]]; then
+#             echo "Invalid File Names"
+#             echo "${invalid_file_names[@]}"
+#         fi
+#     else
+#         echo "Success!!"
+# fi
