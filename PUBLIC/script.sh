771da913952e8baa9721f75a746a159869ab9f77
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-22 16:07:23 +0530

    ERSrdtrfiytswgb

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -38,14 +38,12 @@ for dir in "${unique_dirs[@]}"; do
 done  
 
 for file_name in "${file_names_to_ignore[@]}";do
-    echo ${file_names_to_ignore[@]}
-    echo "-------------------filename egduiyeg-----------------"
-    echo  ${file_name}
+    if 
 done
 
 invalid_file_names=()
 for file_name in "${unique_file_names[@]}"; do
-    if ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
+    if [[! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
           invalid_file_names+=(${file_name})
           echo ${file_name}
           echo "Invalid FileName" 
