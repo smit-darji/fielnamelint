4ae6fe394fecf3603b935028cd82789e9ee196c4
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-23 10:54:46 +0530

    dsygdys

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -1,4 +1,6 @@
-echo "changefile first in sh ${changedf[@]}"
+echo "changefile first in sh "
+echo "${changedf[@]}"
+echo "first in sh"
 echo "$CHANGED_FILES"
 arr=($CHANGED_FILES)
 echo "${arr[@]}"
