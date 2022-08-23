486e348df0aa1dcc68a682e15665ead4ed84e4c8
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-23 11:03:38 +0530

    dsygdys

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -30,35 +30,35 @@ for i in "${!arr[@]}"; do
         fi
     done
 done
-invalid_dirs=()
-for dir in "${unique_dirs[@]}"; do
-    if [[ ! "${dir}" =~ ^[A-Z0-9._]*$ ]]; then
-        invalid_dirs+=(${dir}) 
-    fi
-done  
-invalid_file_names=()
-for file_name in "${unique_file_names[@]}"; do
-    if [[ ! " ${file_names_to_ignore[*]} " =~ " ${arr[i]##*/} " ]]; then
-        exit 0                           
-    fi
-    if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
-        invalid_file_names+=(${file_name})
-        echo ${file_name}
-    fi
-done 
-if [[ ! -z "$invalid_dirs" || ! -z "$invalid_file_names" ]]; 
-    then
-        echo "Failed!!"
-        if [[ ! -z "$invalid_dirs" ]]; then
-            echo "Invalid Directory Names"
-            echo "${invalid_dirs[@]}"
-        fi 
-        if [[ ! -z "$invalid_file_names" ]]; then
-            echo "Invalid File Names"
-            echo "${invalid_file_names[@]}"
-        fi
-        exit 1
-    else
-        echo "Success!!"
-        exit 0
-fi
+# invalid_dirs=()
+# for dir in "${unique_dirs[@]}"; do
+#     if [[ ! "${dir}" =~ ^[A-Z0-9._]*$ ]]; then
+#         invalid_dirs+=(${dir}) 
+#     fi
+# done  
+# invalid_file_names=()
+# for file_name in "${unique_file_names[@]}"; do
+#     if [[ ! " ${file_names_to_ignore[*]} " =~ " ${arr[i]##*/} " ]]; then
+#         exit 0                           
+#     fi
+#     if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
+#         invalid_file_names+=(${file_name})
+#         echo ${file_name}
+#     fi
+# done 
+# if [[ ! -z "$invalid_dirs" || ! -z "$invalid_file_names" ]]; 
+#     then
+#         echo "Failed!!"
+#         if [[ ! -z "$invalid_dirs" ]]; then
+#             echo "Invalid Directory Names"
+#             echo "${invalid_dirs[@]}"
+#         fi 
+#         if [[ ! -z "$invalid_file_names" ]]; then
+#             echo "Invalid File Names"
+#             echo "${invalid_file_names[@]}"
+#         fi
+#         exit 1
+#     else
+#         echo "Success!!"
+#         exit 0
+# fi
