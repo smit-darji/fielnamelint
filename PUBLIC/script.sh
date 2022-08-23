ae57696055a991a5ef6dda4e8c7fafe32262d22e
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-23 11:35:47 +0530

    dsygdys

--- PUBLIC/script.sh
+++ PUBLIC/script.sh
@@ -46,7 +46,7 @@ for file_name in "${unique_file_names[@]}"; do
         #     echo "in contine: ${file_name}"
         #     continue
         #     echo "continue"
-        if [[ ! ${file_name} = ${fileValidator} ]]; then
+        if [[ ! "${file_name}" = [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
             invalid_file_names+=(${file_name})            
             echo "Invalid FileName : ${file_name}"
             exit 1
