8a95e11a60c9dd5727bd47feb850eb6481b72844
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-13 15:50:15 +0530

    as

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -8,10 +8,10 @@ print("Arrys is :::", CHANGED_FILES_ARR)
 
 print ("-------------------------Declare Variables ---------------------------------------")
 # List of file names and directory names which will be ignored while validating
-file_names_to_ignore = ["README.md", ".gitignore"]
+file_names_to_ignore = ("README.md", ".gitignore")
 
 # All files will be ignored inside of this direcories
-directory_names_to_ignore_completely = [".github"]
+directory_names_to_ignore_completely = (".github")
 
 print("file_names_to_ignore is :::", file_names_to_ignore)
 print("directory_names_to_ignore_completely ::", directory_names_to_ignore_completely)
@@ -24,8 +24,8 @@ CHANGED_FILE_LIST = CHANGED_FILES_ARR.split(" ")
 print("type is : temp: ",type(CHANGED_FILE_LIST))
 for i in CHANGED_FILE_LIST:
     for j in directory_names_to_ignore_completely:
-        print("i   is ",i)
-        print("j is: ", j)
+        print("i   is ",CHANGED_FILES_ARR[i])
+        print("j is: ",directory_names_to_ignore_completely[j])
         if CHANGED_FILE_LIST == "{directory_names_to_ignore_completely}":
             print("in if")
             print(CHANGED_FILES_ARR[i])
