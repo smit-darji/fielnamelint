c6c1bb8c6233d1324fc888197d15ac3a815c145c
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-13 16:09:20 +0530

    as

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -12,12 +12,10 @@ print ("-------------------------Declare Variables -----------------------------
 file_names_to_ignore = ["README.md", ".gitignore"]
 
 # All files will be ignored inside of this direcories
-directory_names_to_ignore_completely = [".github"]
+directory_names_to_ignore_completely = [".github", "Terraform"]
 
 print("file_names_to_ignore is :::", file_names_to_ignore)
-print("file_names_to_ignore is :::", type(file_names_to_ignore))
 print("directory_names_to_ignore_completely ::", directory_names_to_ignore_completely)
-print("directory_names_to_ignore_completely ::", type(directory_names_to_ignore_completely))
 print ("-------------------------Removing file of .github directory---------------------------------------")
 
 print("type is :: ",type(CHANGED_FILES_ARR))
@@ -29,17 +27,9 @@ print("CHANGED_FILE_LIST is :: ", CHANGED_FILE_LIST)
 print("type is : temp: ",type(CHANGED_FILE_LIST))
 
 for i in CHANGED_FILE_LIST:
-    print("i is ", i)
     print("i is ", CHANGED_FILE_LIST)
+    print(type(CHANGED_FILE_LIST))
 
 
 
 
-# for i in "${!CHANGED_FILES_ARR[@]}"; do
-#     for j in "${!directory_names_to_ignore_completely[@]}"; do
-#         if [[ "${CHANGED_FILES_ARR[i]}" == "./${directory_names_to_ignore_completely[j]}/"* ]]; then
-#             unset 'CHANGED_FILES_ARR[i]'
-#             break
-#         fi
-#     done
-# done
