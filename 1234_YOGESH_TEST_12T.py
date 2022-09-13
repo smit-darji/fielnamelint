8dbcc1ebce39bf20b72e52a3a353225ffa358b40
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-13 14:53:40 +0530

    as

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -8,10 +8,10 @@ print("Arrys is :::", CHANGED_FILES_ARR)
 
 print ("-------------------------In Python File ---------------------------------------")
 # List of file names and directory names which will be ignored while validating
-file_names_to_ignore = ("README.md", ".gitignore")
+file_names_to_ignore = ["README.md", ".gitignore"]
 
 # All files will be ignored inside of this direcories
-directory_names_to_ignore_completely = (".github")
+directory_names_to_ignore_completely = [".github"]
 
 print("file_names_to_ignore is :::", file_names_to_ignore)
-print("directory_names_to_ignore_completely", directory_names_to_ignore_completely)
+print("directory_names_to_ignore_completely ::", directory_names_to_ignore_completely)
