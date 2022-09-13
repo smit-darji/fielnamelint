74deb4a29a7f70e6a92bbfd29d2fa5dd594c0eee
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-13 16:49:10 +0530

    as

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -1,15 +1,15 @@
-import os
+# import os
 
-print ("-------------------------In Python File ---------------------------------------")
-CHANGED_FILES_ARR = (os.environ.get('CHANGED_FILES'))
-CHANGED_FILE_LIST = CHANGED_FILES_ARR.split(" ")
+# print ("-------------------------In Python File ---------------------------------------")
+# CHANGED_FILES_ARR = (os.environ.get('CHANGED_FILES'))
+# CHANGED_FILE_LIST = CHANGED_FILES_ARR.split(" ")
 # Getting Environment variable named CHANGED_FILES and storing it as an array named CHANGED_FILES_ARR
 
 print("-------------------------Declare Variables ---------------------------------------")
 file_names_to_ignore = ["README.md", ".gitignore"]
 
 
-# CHANGED_FILE_LIST = ['./.github/workflows/test.yml', './.gitignore', './1234_YOGESH_TEST_12T.py', './ABCD/1234_YOGESH_TEST_12T.txt', './Terraform/1234_YOGESH_TEST_12T.txt']
+CHANGED_FILE_LIST = ['./.github/workflows/test.yml', './.gitignore', './1234_YOGESH_TEST_12T.py', './ABCD/1234_YOGESH_TEST_12T.txt', './Terraform/1234_YOGESH_TEST_12T.txt']
 directory_names_to_ignore_completely = [".github", "Terraform"]
 print("CHANGED_FILE_LIST is :: ", CHANGED_FILE_LIST)
 print("-------------------Changed File-----------------------------")
@@ -21,7 +21,7 @@ for i in CHANGED_FILE_LIST:
 print("-------------------unique File-----------------------------")
 unique_file_names=[]
 for i in CHANGED_FILE_LIST:
-    if (unique_file_names != CHANGED_FILE_LIST and file_names_to_ignore != CHANGED_FILE_LIST):
+    if (unique_file_names != "{CHANGED_FILE_LIST}##*/" and file_names_to_ignore != CHANGED_FILE_LIST):
         unique_file_names = unique_file_names+CHANGED_FILE_LIST
         print("Unique FileName",unique_file_names)
 print("-------------------Invalid File-----------------------------")
