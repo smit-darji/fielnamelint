76fabdd11fd62428dcb5f307f544a1289d6584c0
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-13 19:21:03 +0530

    as

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -1,15 +1,15 @@
 import re
 import os
+from itertools import filterfalse
 
 
-print ("-------------------------In Python File ---------------------------------------")
+# print ("-------------------------In Python File ---------------------------------------")
 CHANGED_FILES_ARR = (os.environ.get('CHANGED_FILES'))
 CHANGED_FILE_LIST = CHANGED_FILES_ARR.split(" ")
 # Getting Environment variable named CHANGED_FILES and storing it as an array named CHANGED_FILES_ARR
 file_names_to_ignore = ["README.md", ".gitignore"]
-
-print("Changed FIle Name", CHANGED_FILE_LIST)
 # CHANGED_FILE_LIST = ['./.github/workflows/1.yml', './.github/workflows/script/file_name_validation.sh', './.github/workflows/test.yml', './1234_YOGESH_TEST_12T.py', './ABCD/1234_YOGESH_TEST_12T.txt']
+
 
 directory_names_to_ignore_completely = [".github", "Terraform",".gitignore"]
 directory_names_to_ignore_list = []
@@ -20,8 +20,9 @@ for i in CHANGED_FILE_LIST:
         if (j in i):
             REMOVE_DIR_NAME_LIST.append(i)
 
-print("Remove Directory. ",REMOVE_DIR_NAME_LIST)
-            
+
+CHANGED_FILE_LIST = [i for i in CHANGED_FILE_LIST if i not in REMOVE_DIR_NAME_LIST]
+print("REs is :", str(CHANGED_FILE_LIST))
 # print("-------------------unique File-----------------------------")
 # unique_file_names=[]
 # for i in CHANGED_FILE_LIST:
