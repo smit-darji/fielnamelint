c2c6775daa4896409ae6bbf3bce3f388f177810c
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-13 16:03:38 +0530

    as

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -5,32 +5,37 @@ CHANGED_FILES_ARR = (os.environ.get('CHANGED_FILES'))
 
 # Getting Environment variable named CHANGED_FILES and storing it as an array named CHANGED_FILES_ARR
 print("Arrys is :::", CHANGED_FILES_ARR)
+print("Arrys is :::", type(CHANGED_FILES_ARR))
 
 print ("-------------------------Declare Variables ---------------------------------------")
 # List of file names and directory names which will be ignored while validating
-file_names_to_ignore = ("README.md", ".gitignore")
+file_names_to_ignore = ["README.md", ".gitignore"]
 
 # All files will be ignored inside of this direcories
-directory_names_to_ignore_completely = (".github" ".gitignore")
+directory_names_to_ignore_completely = [".github"]
 
 print("file_names_to_ignore is :::", file_names_to_ignore)
+print("file_names_to_ignore is :::", type(file_names_to_ignore))
 print("directory_names_to_ignore_completely ::", directory_names_to_ignore_completely)
-
+print("directory_names_to_ignore_completely ::", type(directory_names_to_ignore_completely))
 print ("-------------------------Removing file of .github directory---------------------------------------")
 
 print("type is :: ",type(CHANGED_FILES_ARR))
 # Removing file of .github directory as we don't want to run validations on them
+
 CHANGED_FILE_LIST = CHANGED_FILES_ARR.split(" ")
+print("CHANGED_FILE_LIST is :: ", CHANGED_FILE_LIST)
+
 print("type is : temp: ",type(CHANGED_FILE_LIST))
-for i in CHANGED_FILE_LIST:
-    for j in directory_names_to_ignore_completely:
-        print("i   is ",CHANGED_FILE_LIST)
-        print("j is: ",directory_names_to_ignore_completely)
-        if CHANGED_FILE_LIST == directory_names_to_ignore_completely:
-            print("in if")
-            print(CHANGED_FILE_LIST)
-        else:
-            print("in else")
+# for i in CHANGED_FILE_LIST:
+#     for j in directory_names_to_ignore_completely:
+#         print("i   is ",CHANGED_FILES_ARR[i])
+#         print("j is: ",directory_names_to_ignore_completely[j])
+#         if CHANGED_FILE_LIST == "{directory_names_to_ignore_completely}":
+#             print("in if")
+#             print(CHANGED_FILES_ARR[i])
+#         else:
+#             print("in else")
 
 
 
