3e608422ec0aec42fa064326df72e43d95c6ad82
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-13 16:25:19 +0530

    as

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -4,29 +4,21 @@ print ("-------------------------In Python File --------------------------------
 CHANGED_FILES_ARR = (os.environ.get('CHANGED_FILES'))
 
 # Getting Environment variable named CHANGED_FILES and storing it as an array named CHANGED_FILES_ARR
-print("Arrys is :::", CHANGED_FILES_ARR)
-print("Arrys is :::", type(CHANGED_FILES_ARR))
 
-print ("-------------------------Declare Variables ---------------------------------------")
-# List of file names and directory names which will be ignored while validating
+print("-------------------------Declare Variables ---------------------------------------")
 file_names_to_ignore = ["README.md", ".gitignore"]
 
-# All files will be ignored inside of this direcories
-directory_names_to_ignore_completely = [".github", "Terraform"]
-
-print("file_names_to_ignore is :::", file_names_to_ignore)
-print("directory_names_to_ignore_completely ::", directory_names_to_ignore_completely)
-print ("-------------------------Removing file of .github directory---------------------------------------")
-
-print("type is :: ",type(CHANGED_FILES_ARR))
-# Removing file of .github directory as we don't want to run validations on them
-
 CHANGED_FILE_LIST = CHANGED_FILES_ARR.split(" ")
+# CHANGED_FILE_LIST = ['./.github/workflows/1.yml', './.github/workflows/script/file_name_validation.sh',
+#                      './.github/workflows/test.yml', './.gitignore', './1234_YOGESH_TEST_12T.py', './ABCD/1234_YOGESH_TEST_12T.txt', './Terraform/1234_YOGESH_TEST_12T.txt']
+directory_names_to_ignore_completely = [".github", "Terraform"]
 print("CHANGED_FILE_LIST is :: ", CHANGED_FILE_LIST)
 
-print("type is : temp: ",type(CHANGED_FILE_LIST))
+print("type is : temp: ", type(CHANGED_FILE_LIST))
 
 for i in CHANGED_FILE_LIST:
-    print("i is ", CHANGED_FILE_LIST[i])
     for j in directory_names_to_ignore_completely:
-        print("j is ", directory_names_to_ignore_completely)
+        if (j in i):
+            print(f"Matched, CHANGED_FILE_LIST: {i} and directory_names_to_ignore_completely: {j}")
+        else:
+            print("Not matched")
